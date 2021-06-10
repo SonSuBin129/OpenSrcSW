bbc09ec83ad9b9f3c240e9d36daa27e0565bef16
SUBIN_SON <ttokky0129@gmail.com>
2021-06-10 09:47:03 +0900

    week15- class rename3

--- OpenSourceSW/Week15/src/week15/movieAPI.java
+++ OpenSourceSW/Week15/src/week15/movieAPI.java
@@ -14,7 +14,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
  
-public class MovieAPI {
+public class movieAPI {
  
     public static void main(String[] args) throws ParseException {
         String clientId = "IqtWM3oSNWpDiTOg0fvh"; //애플리케이션 클라이언트 아이디값"
